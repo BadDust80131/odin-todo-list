45297052b288cd740fe3ca0e359545231d3e9ff9
BadDust80131 <45376544+BadDust80131@users.noreply.github.com>
2024-08-23 12:52:46 -0700

    JSON Load/Save

---
+++ json.js
@@ -0,0 +1,21 @@
+import { TODO } from "./todo-object";
+
+export function saveProject(project) {
+    localStorage.setItem(project[0],JSON.stringify(project));
+}
+
+export function loadProject(project) {
+    let returnArray = [];
+    let obj = JSON.parse(localStorage.getItem(project[0]));
+    returnArray.push(obj[0]);
+    for (let i = 1; i < obj.length; i++) {
+        let temp = new TODO();
+        temp.title = obj[i].title;
+        temp.description = obj[i].description;
+        temp.dueDate = obj[i].dueDate;
+        temp.priority = obj[i].priority;
+        temp.notes = obj[i].notes;
+        returnArray.push(temp);
+    }
+    return returnArray
+}
--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import './style.css'
 import { TODO } from "./todo-object.js"
+import { saveProject, loadProject } from "./json.js"
 
 const obj = new TODO();
 obj.notes = "help"
 
 console.log(obj)
+
+const testProject = [
+  "Test Project",
+  new TODO(),
+  new TODO(),
+  new TODO(),
+]
+console.log(testProject)
+
+saveProject(testProject);
+console.log(loadProject(testProject))
