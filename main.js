c8ccfe4bedf0c847c36e7f0a7cf9935f3007bef1
BadDust80131 <45376544+BadDust80131@users.noreply.github.com>
2024-08-23 12:31:40 -0700

    Basic TODO Class

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import './style.css'
+import { TODO } from "./todo-object.js"
 
+const obj = new TODO();
+obj.notes = "help"
 
+console.log(obj)
---
+++ todo-object.js
@@ -0,0 +1,11 @@
+import { addDays } from "date-fns";
+
+export class TODO {
+    constructor(title = "Default Title", description = "Default Description", dueDate = addDays(new Date(),1), priority = false, notes = "Default Notes") {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}
